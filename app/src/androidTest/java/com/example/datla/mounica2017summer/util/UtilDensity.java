b5b8fb056f4476fd727be9d7cfab75d0ead2883d
mounicadatla <mounica.datla77@gmail.com>
2017-06-18 15:33:53 -0400

    13AdvanceListView2

--- app/src/androidTest/java/com/example/datla/mounica2017summer/util/UtilDensity.java
+++ app/src/androidTest/java/com/example/datla/mounica2017summer/util/UtilDensity.java
@@ -1,27 +1,24 @@
 package com.example.datla.mounica2017summer.util;
-import android.content.Context;
 /**
  * Created by mounicadatla on 6/17/17.
  */
 
-public class UtilDensity {
+import android.content.Context;
 
+/**
+ * Created by YoungH on 2/8/17.
+ */
 
+      public class UtilDensity {
 
-    /**
-     * Created by YoungH on 2/8/17.
-     */
-
-
-        public static int dip2px(Context context, float dpValue) {
-            final float scale = context.getResources().getDisplayMetrics().density;
-            return (int) (dpValue * scale + 0.5f);
-        }
-
-        public static int px2dip(Context context, float pxValue) {
-            final float scale = context.getResources().getDisplayMetrics().density;
-            return (int) (pxValue / scale + 0.5f);
-        }
-
+    public static int dip2px(Context context, float dpValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
     }
 
+    public static int px2dip(Context context, float pxValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+
+}
---
+++ app/src/test/java/com/example/datla/mounica2017summer/util/UtilDensityTest.java
@@ -0,0 +1,8 @@
+package com.example.datla.mounica2017summer.util;
+
+/**
+ * Created by mounicadatla on 6/18/17.
+ */
+public class UtilDensityTest {
+
+}
