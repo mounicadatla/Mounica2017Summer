27ef1032014930675f836d080aa6320fa44b7290
Kishore Kalidindi <kkishorekr@gmail.com>
2017-06-19 10:41:03 -0400

    part of quiz1

--- app/src/main/java/com/example/datla/mounica2017summer/fragment/WorkFragment.java
+++ app/src/main/java/com/example/datla/mounica2017summer/fragment/WorkFragment.java
@@ -1,13 +1,26 @@
 package com.example.datla.mounica2017summer.fragment;
 
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
+
+import com.example.datla.mounica2017summer.AdvanceListViewActivity;
+import com.example.datla.mounica2017summer.NotificationActivity;
+import com.example.datla.mounica2017summer.PatchActivity;
 import com.example.datla.mounica2017summer.R;
+import com.example.datla.mounica2017summer.ScaleTypeActivity;
+import com.example.datla.mounica2017summer.adapter.ListNormalAdapter;
+import com.example.datla.mounica2017summer.viewPagerActivity;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,10 +36,20 @@ public class WorkFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private final Context context;
+    private final ArrayList<String> contentList;
+    private ListView listView;
+
 
 
     public WorkFragment() {
         // Required empty public constructor
+        context = getContext();
+        contentList = new ArrayList<String>();
+        contentList.add("the fragment is used for");
+        contentList.add("three buttons");
+        contentList.add("Submit");
+
     }
 
     /**
@@ -60,7 +83,38 @@ public class WorkFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_work, container, false);
+        View view = inflater.inflate(R.layout.fragment_work, container, false);
+        listView = (ListView) view.findViewById(R.id.fragment_work_lv);
+        ListNormalAdapter adapter = new ListNormalAdapter(this.getContext(),contentList);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position) {
+                    case 0:
+                        Intent intent = new Intent(getActivity(), viewPagerActivity.class);
+                        startActivity(intent);
+                        break;
+                    case 1:
+                        Intent intent1 = new Intent(getActivity(), ScaleTypeActivity.class);
+                        startActivity(intent1);
+                        break;
+                    case 2:
+                        Intent intent2 = new Intent(getActivity(),PatchActivity.class);
+                        startActivity(intent2);
+                        break;
+                    case 3:
+                        Intent intent3 = new Intent(getActivity(),NotificationActivity.class);
+
+                        startActivity(intent3);
+                        break;
+                }
+            }
+        });
+
+        return view;
+//        return inflater.inflate(R.layout.fragment_work, container, false);
     }
 
 }
