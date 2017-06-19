95f7b4b44435615276b457450ab729a07ba45382
mounicadatla <mounica.datla77@gmail.com>
2017-06-19 10:06:37 -0400

    All changes are working Jun 18th Sunday 11.30pm.

---
+++ app/src/main/java/com/example/datla/mounica2017summer/adapter/ViewFragmentStateAdapter.java
@@ -0,0 +1,14 @@
+package com.example.datla.mounica2017summer.adapter;
+/**
+import FragmentStateAdapter;
+
+
+ * Created by mounicadatla on 6/19/17.
+
+
+public class ViewFragmentStateAdapter extends FragmentStateAdapter {
+ private final ArrayList<Pair<String, Fragment>>
+ </String,>
+
+}
+*/
